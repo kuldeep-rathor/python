36a066c543b90f0d2f3e6fd0c6353be0e521ad18
kuldeep-rathor <kuldeeprathor2201@gmail.com>
2025-11-26 15:35:55 +0530

    variables and data types.

---
+++ variables_dataTypes.py
@@ -0,0 +1,33 @@
+print("Data types and variables in python")
+
+a=123456 #Integer variable
+
+b="Kuldeep" #String variable
+print(a)
+print(b)
+
+c=True
+print(c) #Boolean variable
+
+d=None #None type variable
+print(d)
+
+
+# print(a+b) #Concatenation with same datatypes
+
+print("The type of a is:", type(a))
+print("The type of a is:", type(b))
+print("The type of a is:", type(c))
+print("The type of a is:", type(d))
+
+#Built in data types in python
+
+
+e=complex(2,3) #complex numbers
+print("The complex number is:", e)
+
+f=23.45 #float data type
+print("The float number is:", f)    
+
+
+# string , boolean ,  float , integer , complex , none type 
