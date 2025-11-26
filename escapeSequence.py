159c8cc271942c2ea828dc1430b0d2f2cbe2e798
kuldeep-rathor <kuldeeprathor2201@gmail.com>
2025-11-26 15:25:15 +0530

    Comment, multiple line comments , escape sequence characters

---
+++ escapeSequence.py
@@ -0,0 +1,26 @@
+print("Hello what are you doing" \
+"Hope you are doing great/not bad\tGoodbye!")
+
+
+
+#This is valid print statement , but not recommended
+print("Hello what are you doing""Hope you are doing great/not bad\tGoodbye!")
+
+print("Same \"line\" print") #Same line print
+
+
+'''
+
+Multiple line comment using triple quotes.
+This is useful for documentation purpose.
+you can write multiple lines of comments here.
+
+
+'''
+
+
+print("Hey" , 6 ,7) #print with multiple arguments 
+print("Hey" , 6 , 7, sep="~"), #print with multiple arguments with custom separator 
+print("Hey" , 6 , 7, sep="~", end="**")
+
+print("Next print statement")
